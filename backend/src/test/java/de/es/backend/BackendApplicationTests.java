2c1a6f145c6a6ff87bfdfd949327cfcc80f593f6
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-11-03 10:16:08 +0100

    Add backend module

---
+++ backend/src/test/java/de/es/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.es.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
